5eab835b28058c1fb79bd61bb69c69821acc18e4
TamannaDafedar <148558424+TamannaDafedar@users.noreply.github.com>
2024-04-03 14:53:33 +0530

    SQL2_10

---
+++ Varchar & nVarchar.sql
@@ -0,0 +1,98 @@
+﻿--Varchar & Nvarchar difference
+--1)varchar
+declare @Name varchar(50), @MName varchar(50)
+set @Name = 'Zoya'
+print @Name
+
+set @Name = N'झोया'
+print @Name
+/*Zoya
+  ????*/
+ 
+--2)nVarchar
+
+declare @Name varchar(50), @MName nvarchar(50)
+set @Name = 'Zoya'
+print @Name
+
+set @MName = N'झोया'
+print @MName
+
+/*Zoya
+  झोया*/
+--nvarchar store any language code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
